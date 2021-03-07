d662344b2ece3dd68a75015cddbf4ce9bd571414
anel <68648675+anelnjk@users.noreply.github.com>
2021-03-07 22:01:50 +0600

    Update ChooseTicket.java

--- ChooseTicket.java
+++ ChooseTicket.java
@@ -1,98 +1,106 @@
-import java.util.*;
 import java.sql.*;
+import java.util.Scanner;
 
-public class Application
-{
-    public static void main(String[] args)
+public class ChooseTicket{
+    Connection con = null;
+    ResultSet resultSet = null;
+    Statement statement = null;
+    BuyTicket buyTicket = new BuyTicket();
+    Scanner in = new Scanner(System.in);
+
+    public void chooseTicket(String city1, String city2) throws SQLException
     {
-        //connection to our DBMS
+        Flight flight = new Flight();
+
         String connectionUrl = "jdbc:postgresql://localhost:5432/AirlineSystem";
         String username = "postgres";
         String password = "postgres";
-        Connection con = null;
-        ResultSet resultSet = null;
-        Statement statement = null;
 
-        Scanner in = new Scanner(System.in);
+        con = DriverManager.getConnection(connectionUrl, username, password);
 
-        Application user = new Application();
-        ChooseTicket chooseTicket = new ChooseTicket();
-        Passenger passenger = new Passenger();
-
-        //start of our application
-        System.out.println("* It is application for airline system *");
-
-        boolean choice = false; //indicates if user has made a desicion or not
-        int decision = 0; //desicion to buy ticket/quit program
-        String city1 = null; //city user departs from
-        String city2 = null; //city user departs to
-
-        do
+        statement = con.createStatement();
+        resultSet = statement.executeQuery("select * from flight where depart_from='"+city1+"' and depart_to = '"+city2+"'");
+        //search for direct flight from city1 to city2
+        if(resultSet!=null && resultSet.next() == true)
         {
-            try
+            while (resultSet.next())
             {
-                System.out.println("1. Buy ticket");
-                System.out.println("2. Quit from program");
-                decision = in.nextInt();
-                in.nextLine();
-
-                switch (decision)
-                {
-                    case 1:
-                        System.out.println("Enter city you depart from: ");
-                        city1 = in.nextLine();
-                        System.out.println("Enter city you arrive to");
-                        city2 = in.nextLine();
-                        break;
-                    case 2:
-                        return;
-                    default:
-                        System.out.println("Incorrect number was inputted, write correct number (1/2)");
-
-                }
-                choice = true;
-            }
-            catch(InputMismatchException exception) //exception of wrong input
-            {
-                System.out.println("Please input an integer");
-                System.out.println("Required int, instead of: " + in.nextLine());
-            }
-        } while (!choice || decision < 1 && decision > 3);
-
-        try{
-            Class.forName("org.postgresql.Driver"); //download driver for DBMS
-            con = DriverManager.getConnection(connectionUrl, username, password);
-            statement = con.createStatement();
-
-            resultSet = statement.executeQuery("select flight.flight_id, depart_from,depart_to,ticket.ticket_id,class_vip,date_from,date_to from flight,ticket where ticket.flight_id=flight.flight_id and depart_from='"+city1+"' and depart_to = '"+city2+"'");
-            //query to select ticket with appropriate city1 and city2
-            System.out.println("Availible tickets with corresponding coordinates are:\nif(t) = business class, if(f)=econom class\n");
-            //printing those ticket(s) and show them to user
-
-            while (resultSet.next()){
-                System.out.println( resultSet.getInt("ticket_id")+ " "
-                        + resultSet.getString("depart_from")+" "+
+                System.out.println( resultSet.getInt("flight_id")+ " "+
+                        resultSet.getInt("airplane_id")+ " "+
+                        resultSet.getString("code")+ " "+
+                        resultSet.getString("depart_from")+ " "+
                         resultSet.getString("depart_to")+ " "+
-                        resultSet.getString("class_vip")+" "+
+                        resultSet.getString("company_name")+ " "+
                         resultSet.getString("date_from")+ " "+
                         resultSet.getString("date_to"));
-            }
-            chooseTicket.chooseTicket(city1, city2);
-            //go and choose ticket that user wants
 
-        } catch(SQLException | ClassNotFoundException exception){
-            exception.printStackTrace();
-        } finally{
-            try{
-                resultSet.close();
-                statement.close();
-                con.close();
-            } catch(SQLException exception){
-                exception.printStackTrace();
+            }
+            System.out.println("\nEnter ID of ticket you want to choose:");
+            int ticket_id = in.nextInt();
+            //input id of ticket with suitable info for user
+            ResultSet validTicket = statement.executeQuery("select * from ticket where ticket_id="+ticket_id);
+            if(validTicket.next()!=true){
+                //in case invalid id was input, print the message
+                System.out.println("This ticket does not exist.");
+                return;
+            } else{
+                buyTicket.buyTicket(ticket_id);
+                //passing id of ticket and but it
             }
         }
-            chooseTicket.buyTicket.showTicket();
-            //printing ticket that user has chosen and bought
+        else
+            //in case there is no direct ticket from city1 to city2
+        {
+            statement = con.createStatement();
+            resultSet = statement.executeQuery("SELECT * from flight where depart_to = '" + city2 + "'");
+            //find flight FROM city1
+
+            String connectCity;
+            int counter = 1;
+            int idFirst = 0;
+            int idSecond = 0;
+            while(resultSet.next() )
+            {
+                connectCity = resultSet.getString("depart_from");
+                //setting connector city as city2 of our flight
+                //and search for city with depart_from as connector city
+
+                Statement connectStatement = con.createStatement();
+                ResultSet resultSetConnectingTwoCities = connectStatement.executeQuery("SELECT * from flight where depart_to = '" + connectCity + "' and depart_from = '" + city1+"'");
+
+                if(resultSetConnectingTwoCities != null && resultSetConnectingTwoCities.next() == true)
+                {
+                    System.out.println("There is special way to go there. And it is transfer way, like above. Way №" + counter);
+
+                    System.out.println( resultSet.getInt("flight_id")+ " "+
+                            resultSet.getInt("airplane_id")+ " "+
+                            resultSet.getString("code")+ " "+
+                            resultSet.getString("depart_from")+ " "+
+                            resultSet.getString("depart_to")+ " "+
+                            resultSet.getString("company_name")+ " "+
+                            resultSet.getString("date_from")+ " "+
+                            resultSet.getString("date_to"));
+                    idFirst = resultSet.getInt("flight_id");
+                    System.out.println( resultSetConnectingTwoCities.getInt("flight_id")+ " "+
+                            resultSetConnectingTwoCities.getInt("airplane_id")+ " "+
+                            resultSetConnectingTwoCities.getString("code")+ " "+
+                            resultSetConnectingTwoCities.getString("depart_from")+ " "+
+                            resultSetConnectingTwoCities.getString("depart_to")+ " "+
+                            resultSetConnectingTwoCities.getString("company_name")+ " "+
+                            resultSetConnectingTwoCities.getString("date_from")+ " "+
+                            resultSetConnectingTwoCities.getString("date_to"));
+                    idSecond = resultSetConnectingTwoCities.getInt("flight_id");
+                }
+                counter++;
+                buyTicket.buyTicket(idFirst, idSecond); //pass two tickets and buy them
+            }
+            if(counter == 1)
+            {
+                System.out.println("There is no possible variants.");
+            }
+            return;
+        }
 
     }
+
 }
